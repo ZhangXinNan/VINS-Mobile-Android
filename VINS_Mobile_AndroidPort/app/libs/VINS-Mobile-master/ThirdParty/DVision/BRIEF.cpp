ae24a38d2c8d3d07027007b7bf0560127703cdb0
ZhangXinNan <zhangxin19870504@163.com>
2019-04-23 09:22:40 +0800

    build with opencv 4.1.0

--- VINS_Mobile_AndroidPort/app/libs/VINS-Mobile-master/ThirdParty/DVision/BRIEF.cpp
+++ VINS_Mobile_AndroidPort/app/libs/VINS-Mobile-master/ThirdParty/DVision/BRIEF.cpp
@@ -51,7 +51,8 @@ void BRIEF::compute(const cv::Mat &image,
     cv::Mat aux;
     if(image.depth() == 3)
     {
-      cv::cvtColor(image, aux, CV_RGB2GRAY);
+      cv::cvtColor(image, aux, cv::COLOR_RGB2GRAY); // opencv 4.1.0
+//      cv::cvtColor(image, aux, CV_RGB2GRAY);
     }
     else
     {
